6cd93717082ac2d8ed7bc9dfc120eeeb1edbeb9d
Rohan Hangal <rohanhangal@gmail.com>
2022-11-06 13:32:24 -0800

    completed task 7

--- index.js
+++ index.js
@@ -293,10 +293,12 @@ Use addArtist to do the following:
 5. Add the newly created object to the copied array, then return the copied array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, insName, insYears, insGenre, insNationality, insBio) {
+  const addArtistArray = [...array];
+  addArtistArray.push({"name": insName, "years": insYears, "genre": insGenre, "nationality": insNationality, "bio": insBio});
+  return addArtistArray
 }
-
+console.log(addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
